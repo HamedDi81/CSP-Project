ef814569e14e73bcc199158f896f046dd8f43c7a
impundit <apunditboy@gmail.com>
2023-02-04 01:33:55 +0330

    add test project

---
+++ test.py
@@ -0,0 +1,130 @@
+class Node:
+    salons: list['Salon']
+
+    def __init__(self, salons: list['Salon']) -> None:
+        self.salons = salons
+
+    def addSalon(self, salon: 'Salon') -> None:
+        self.salons.append(salon)
+    
+    def copy(self) -> 'Node':
+        newNode = Node([])
+        for salon in self.salons:
+            newNode.addSalon(salon.copy())
+        return newNode
+
+class Salon:
+    neighbors: list['Salon']
+    domain: list[int]
+
+    def __init__(self, neighbors: list['Salon'], domain: list[int]) -> None:
+        self.neighbors = neighbors
+        self.domain = domain
+
+    def addDomain(self, group: int):
+        self.domain.append(group)
+
+    def addNeighbor(self, salon: 'Salon'):
+        self.neighbors.append(salon)
+
+    def copy(self) -> 'Salon':
+        return Salon(self.neighbors, self.domain.copy())
+
+
+def MRV(node: Node) -> int:
+    for i in range(len(node.salons)):
+        if len(node.salons[i].domain) > 1:
+            return i
+
+
+def LCV(node: Node, salon: int) -> list[int]:
+    return node.salons[salon].domain
+
+
+def forward_checking(node: Node, salon: int, group: int) -> None:
+    node.salons[salon].domain = [group]
+
+
+def AC3(node: Node) -> None:
+    pass
+
+
+def isSatisfy(node: Node, salon: int, group: int) -> bool:
+    for neighbor in node.salons[salon].neighbors:
+        if neighbor.domain == [group]:
+            return False
+
+    return True
+
+
+def isComplete(node: Node) -> bool:
+    for salon in node.salons:
+        if len(salon.domain) != 1:
+            return False
+
+    return True
+
+
+def isFailure(node: Node) -> bool:
+    for salon in node.salons:
+        if len(salon.domain) == 0:
+            return True
+
+    return False
+
+
+def backtracking(root: Node) -> Node | None:
+    stack = [root]
+
+    while stack:
+        state = stack.pop()
+
+        if isComplete(state):
+            return state
+
+        salon =  MRV(state)
+        for group in LCV(state, salon)[-1::-1]:
+            if isSatisfy(state, salon, group):
+                child = state.copy()
+                forward_checking(child, salon, group)
+                stack.append(child)
+        
+
+    return None
+
+
+def getInput():
+    n, m = (int(x) for x in input().split())
+    salons : list['Salon'] = []
+    for i in range(n):
+        salons.append(Salon([], []))
+
+    for i in range(m):
+        for salon in input().split():
+            salon = int(salon) - 1
+            salons[salon].addDomain(i)
+
+    e = int(input())
+    for i in range(e):
+        a, b = (int(x) - 1 for x in input().split())
+        salons[a].addNeighbor(salons[b])
+        salons[b].addNeighbor(salons[a])
+    
+    return salons
+
+
+def main() -> None:
+    salons = getInput()
+
+    problem = Node(salons)
+    result = backtracking(problem)
+
+    if result == None:
+        print('No')
+        return None
+
+    for salon in result.salons:
+        print(salon.domain[0] + 1, end = ' ')
+
+if __name__ == '__main__':
+    main()
