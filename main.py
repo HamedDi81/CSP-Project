3bd9b24743501e3cf39661784e3d1f1f65115c43
impundit <apunditboy@gmail.com>
2023-02-05 04:53:24 +0330

    last commit

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import sys
+
+
 class Node:
     salons: list['Salon']
 
@@ -7,19 +9,20 @@ class Node:
 
     def addSalon(self, salon: 'Salon') -> None:
         self.salons.append(salon)
-    
+
     def copy(self) -> 'Node':
         newNode = Node([])
         for salon in self.salons:
             newNode.addSalon(salon.copy())
         return newNode
 
+
 class Salon:
     neighbors: list[int]
     domain: list[int]
     assigned: bool
 
-    def __init__(self, neighbors: list['Salon'], domain: list[int], assigned = False) -> None:
+    def __init__(self, neighbors: list['Salon'], domain: list[int], assigned=False) -> None:
         self.neighbors = neighbors
         self.domain = domain
         self.assigned = assigned
@@ -44,13 +47,13 @@ def MRV(node: Node) -> int:
             if len(node.salons[i].domain) < min_domain:
                 min_domain = len(node.salons[i].domain)
                 index = i
-    
+
     for i in range(len(node.salons)):
-            if len(node.salons[i].domain) == min_domain and not node.salons[i].assigned:
-                if len(node.salons[i].neighbors) > max_neighbor:
-                    max_neighbor = len(node.salons[i].neighbors)
-                    index = i
-        
+        if len(node.salons[i].domain) == min_domain and not node.salons[i].assigned:
+            if len(node.salons[i].neighbors) > max_neighbor:
+                max_neighbor = len(node.salons[i].neighbors)
+                index = i
+
     return index
 
 
@@ -69,13 +72,14 @@ def LCV(node: Node, salon: int) -> list[int]:
                     score = -1
                     i -= 1
                     break
-                else : score += 1
+                else:
+                    score += 1
         if score != -1:
             domain_sort[domain_sort.index(d)] = (score, d)
         score = 0
         i += 1
 
-    domain_sort.sort(key = lambda x: x[0])
+    domain_sort.sort(key=lambda x: x[0])
     for i in domain_sort:
         domain_sort[domain_sort.index(i)] = i[1]
 
@@ -88,34 +92,36 @@ def forward_checking(node: Node, salon: int, group: int) -> None:
         if group in neighbor.domain:
             neighbor.domain.remove(group)
 
-def constraint_varibale(node:Node) -> list[tuple['Salon', 'Salon']]:
-    constraint=list()
+
+def constraint_varibale(node: Node) -> list[tuple['Salon', 'Salon']]:
+    constraint = list()
     for salon in node.salons:
         for neighbor in salon.neighbors:
             neighbor = node.salons[neighbor]
-            constraint.append((salon,neighbor))
+            constraint.append((salon, neighbor))
     return constraint
 
 
-def AC3(node: Node , queue:list=None) -> bool:
+def AC3(node: Node, queue: list = None) -> bool:
     if queue == None:
         queue = constraint_varibale(node)
 
     while queue:
-        (salon_i,salon_j)= queue.pop(0)
+        (salon_i, salon_j) = queue.pop(0)
         if remove_inconsistent_values(salon_i, salon_j):
-            if len(salon_i.domain) == 0: 
+            if len(salon_i.domain) == 0:
                 return False
-                
+
             for salon_k in salon_i.neighbors:
                 salon_k = node.salons[salon_k]
-                if (salon_k, salon_i) not in queue: #TODO debug test
+                if (salon_k, salon_i) not in queue:
                     queue.append((salon_k, salon_i))
-                    
-    return True
-      
 
-def remove_inconsistent_values(cell_i : Salon, cell_j : Salon) -> bool: #returns true if a value is removed
+    return True
+
+
+# returns true if a value is removed
+def remove_inconsistent_values(cell_i: Salon, cell_j: Salon) -> bool:
     removed = False
 
     for value in cell_i.domain:
@@ -123,8 +129,9 @@ def remove_inconsistent_values(cell_i : Salon, cell_j : Salon) -> bool: #returns
         if cell_j.domain == [value]:
             cell_i.domain.remove(value)
             removed = True
-        
+
     return removed
+
 
 def isSatisfy(node: Node, salon: int, group: int) -> bool:
     for neighbor in node.salons[salon].neighbors:
@@ -151,7 +158,7 @@ def isFailure(node: Node) -> bool:
     return False
 
 
-def backtracking(root: Node, mode: int= 1) -> Node | None:
+def backtracking(root: Node, mode: int = 1) -> Node | None:
     stack = [(root, None, None)]
 
     while stack:
@@ -183,54 +190,13 @@ def backtracking(root: Node, mode: int= 1) -> Node | None:
                 child.salons[salon].domain = [group]
                 child.salons[salon].assigned = True
                 stack.append((child, salon, group))
-        
 
     return None
 
 
-def testGenerator() -> list['Salon']:
-    from random import randint as rant, choices
-    n = int(input('n = '))
-    m = int(input('m = '))
-    e = int(input('e = '))
-
-    salons : list['Salon'] = []
-    for i in range(n):
-        salons.append(Salon([], []))
-
-    L = [str(i) for i in range(1,n+1)]
-    for i in range(m):
-        chs = choices(L,k=1 if rant(1,4) == 1 else rant(2,10))
-        for k in range(len(chs)):
-            j = k+1
-            while j < len(chs):
-                if chs[k] == chs[j]:
-                    chs.pop(j)
-                    j -= 1
-                j += 1
-        for salon in chs:
-            salon = int(salon) - 1
-            salons[salon].addDomain(i)
-
-    # e = rant(m*2,m*(m-1)/2)
-
-    edges = set()
-    while len(edges) != e:
-        t = tuple(x for x in choices(L,k=2))
-        t_e = (t[1],t[0])
-        if t_e not in edges and t[0] != t[1]:
-            edges.add(t)
-            
-    for edge in edges:
-        a, b = (int(edge[0]) - 1, int(edge[1]) -1)
-        salons[a].addNeighbor(b)
-        salons[b].addNeighbor(a)
-    
-    return salons
-
 def getInput():
     n, m = (int(x) for x in input().split())
-    salons : list['Salon'] = []
+    salons: list['Salon'] = []
     for i in range(n):
         salons.append(Salon([], []))
 
@@ -244,13 +210,12 @@ def getInput():
         a, b = (int(x) - 1 for x in input().split())
         salons[a].addNeighbor(b)
         salons[b].addNeighbor(a)
-    
+
     return salons
 
 
 def main() -> None:
-    # salons = getInput()
-    salons = testGenerator()
+    salons = getInput()
 
     problem = Node(salons)
 
@@ -260,34 +225,38 @@ def main() -> None:
     start = datetime.now()
     result = backtracking(_problem, 1)
     end = datetime.now()
-    print('\n\ntime =', end-start)
-    if result == None:
-        print('No')
-    else:
-        for salon in result.salons:
-            print(salon.domain[0] + 1, end = ' ')
 
-    _problem = problem.copy()
-    start = datetime.now()
-    result = backtracking(_problem, 0)
-    end = datetime.now()
     print('\n\ntime =', end-start)
     if result == None:
         print('No')
     else:
         for salon in result.salons:
-            print(salon.domain[0] + 1, end = ' ')
+            print(salon.domain[0] + 1, end=' ')
 
-    _problem = problem.copy()
-    start = datetime.now()
-    result = backtracking(_problem, 2)
-    end = datetime.now()
-    print('\n\ntime =', end-start)
-    if result == None:
-        print('No')
-    else:
-        for salon in result.salons:
-            print(salon.domain[0] + 1, end = ' ')
+    # _problem = problem.copy()
+    # start = datetime.now()
+    # result = backtracking(_problem, 0)
+    # end = datetime.now()
+
+    # print('\n\ntime =', end-start)
+    # if result == None:
+    #     print('No')
+    # else:
+    #     for salon in result.salons:
+    #         print(salon.domain[0] + 1, end=' ')
+
+    # _problem = problem.copy()
+    # start = datetime.now()
+    # result = backtracking(_problem, 2)
+    # end = datetime.now()
+
+    # print('\n\ntime =', end-start)
+    # if result == None:
+    #     print('No')
+    # else:
+    #     for salon in result.salons:
+    #         print(salon.domain[0] + 1, end=' ')
+
 
 if __name__ == '__main__':
     main()
